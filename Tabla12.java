22d02315347ea3342586476646dbdd69900ea980
robertogago1980git <gago1980@hotmail.es>
2026-05-19 15:38:36 +0200

    github tabla12

--- Tabla12.java
+++ Tabla12.java
@@ -1,6 +1,6 @@
 public class Tabla12 { 
 public static void main(String args[]) { 
-for(int i = 0; i <= 12; i++) { 
+for(int i = 0; i <= 20; i++) { 
 System.out.println("10 * "+ i + " = " + 10 * i + "\n"); 
 } 
 } 
