443ba7ce3f065db17b2387a54b5c2d55a28253b1
Juan <juan@gmail.com>
2026-05-19 15:04:19 +0200

    inicio

---
+++ Tabla10.java
@@ -0,0 +1,7 @@
+public class Tabla10 { 
+    public static void main(String args[]) { 
+    for(int i = 0; i <= 10; i++) { 
+    System.out.println("10 * "+ i + " = " + 10 * i + "\n"); 
+    } 
+    } 
+    } 
